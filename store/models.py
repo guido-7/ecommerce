7d8b622a74c21cdceff8b13874ed3a173427e49c
Guido <guidor0033@gmail.com>
2025-07-07 16:08:19 +0200

    add initial project setup with .gitignore, build script, and updated requirements for cloud storage and database configuration

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from cloudinary.models import CloudinaryField
 from django.db import models
 from django.utils import timezone
 
@@ -49,7 +50,7 @@ class Product(models.Model):
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='products/')
+    image = CloudinaryField('image')
 
     def __str__(self):
         return f"Image for {self.product.name}"
